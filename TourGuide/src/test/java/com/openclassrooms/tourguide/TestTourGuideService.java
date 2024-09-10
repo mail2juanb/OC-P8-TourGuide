d4acb17a962e524a9711d805cfd67efdf2a785c9
Scarounine <paulinelesueur93@gmail.com>
2024-09-10 16:44:29 +0200

    test: getNearbyAttractions test passes

--- TourGuide/src/test/java/com/openclassrooms/tourguide/TestTourGuideService.java
+++ TourGuide/src/test/java/com/openclassrooms/tourguide/TestTourGuideService.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.List;
 import java.util.UUID;
 
+import com.openclassrooms.tourguide.DTO.AttractionDTO;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -92,7 +93,6 @@ public class TestTourGuideService {
 		assertEquals(user.getUserId(), visitedLocation.userId);
 	}
 
-	@Disabled // Not yet implemented
 	@Test
 	public void getNearbyAttractions() {
 		GpsUtil gpsUtil = new GpsUtil();
@@ -101,9 +101,10 @@ public class TestTourGuideService {
 		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
 
 		User user = new User(UUID.randomUUID(), "jon", "000", "jon@tourGuide.com");
-		VisitedLocation visitedLocation = tourGuideService.trackUserLocation(user);
 
-		List<Attraction> attractions = tourGuideService.getNearByAttractions(visitedLocation);
+		tourGuideService.trackUserLocation(user);
+
+		List<AttractionDTO> attractions = tourGuideService.getFiveNearbyAttractions(user);
 
 		tourGuideService.tracker.stopTracking();
 
